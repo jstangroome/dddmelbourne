648ba4c0a9e6220dbc7b43e260b73ae6724be02a
Aaron Powell <me@aaron-powell.com>
2016-03-27 19:14:51 +1100

    Porting for dddsyd

--- DevDevDev/Models/EventConfig.cs
+++ DevDevDev/Models/EventConfig.cs
@@ -2,8 +2,8 @@
 {
     public static class EventConfig
     {
-        public const string EventTitle = "DDD Melbourne 7";
-        public const string EventDateDescription = "Sat Aug 13th 2016";
+        public const string EventTitle = "DDD Sydney";
+        public const string EventDateDescription = "Coming Soon 2016";
         public const int TotalVotes = 10;
     }
 }
