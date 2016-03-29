faecd04b9b5750a945e2ff6a96f625c14dbedf9f
Aaron Powell <me@aaron-powell.com>
2016-03-29 17:41:53 +1100

    Setting the date

--- DevDevDev/Models/EventConfig.cs
+++ DevDevDev/Models/EventConfig.cs
@@ -3,7 +3,7 @@
     public static class EventConfig
     {
         public const string EventTitle = "DDD Sydney";
-        public const string EventDateDescription = "Coming Soon 2016";
+        public const string EventDateDescription = "28 May 2016";
         public const int TotalVotes = 10;
     }
 }
